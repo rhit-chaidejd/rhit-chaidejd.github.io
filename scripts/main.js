76f9bc15722784d0db6e12cc9d1459b3f9e0f634
rhit-chaidejd <chaidejd@rose-hulman.edu>
2023-09-22 23:33:17 -0400

    Homework1

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,6 @@
 let counter = 0;
+let colorCode = 'dc3545';
+let color = 'red';
 
 main = function() {
     console.log("Ready");
@@ -19,10 +21,48 @@ main = function() {
         counter = counter + 1;
         updateView();
     }
+
+    document.querySelector("#blueButton").onclick = (event) => {
+        console.log("blue button");
+        colorCode = '0d6efd';
+        color = 'blue';
+        updateColor(colorCode, color);
+    }
+
+    document.querySelector("#redButton").onclick = (event) => {
+        console.log("red button");
+        colorCode = 'dc3545';
+        color = 'red';
+        updateColor(colorCode, color);
+    }
+
+    document.querySelector("#greenButton").onclick = (event) => {
+        console.log("green button");
+        colorCode = '198754';
+        color = 'green';
+        updateColor(colorCode, color);
+    }
+
+    document.querySelector("#purpleButton").onclick = (event) => {
+        console.log("purple button");
+        colorCode = '800080';
+        color = 'purple';
+        updateColor(colorCode, color);
+    }
+
+    
 }
 
 updateView = function() {
-    document.querySelector("#counterText").innerHTML = `Count = ${counter}`
+    document.querySelector("#counterText").innerHTML = `${counter}`
 }
+
+updateColor = function(anyColorCode, anyColor) {
+    const box = getElementById("favoriteColorBox");
+    box.style = `background-color:#${anyColorCode}`
+    document.querySelector("#favoriteColorBox").innerHTML = `${anyColor}`
+}
+
+
 
 main();
